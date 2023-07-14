6df52e337bcf4c377bd683b8474fcb910724b81f
Mia21Mihaela <78301504+Mia21Mihaela@users.noreply.github.com>
2023-07-14 18:24:26 +0300

    Update teste-linkedin.cy.js

--- cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
+++ cypress/e2e/TesteLinkedIn/teste-linkedin.cy.js
@@ -3,8 +3,8 @@ describe('On LinkedIn', () => {
     it('I can login', () => {
         cy.visit('https://linkedin.com');
         cy.get('.nav__button-secondary').click();
-        cy.get('#username').type('mihaela.lisman21@gmail.com');
-        cy.get('#password').type('Qwertyhn21.');
+        cy.get('#username').type('mail.exaple@gmail.com');
+        cy.get('#password').type('example');
         cy.get('.btn__primary--large').click();
         cy.get('#ember15').should('exist');
     })
